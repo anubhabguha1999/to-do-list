5a9d3b8209b5278d6477a4ef4aff30c76843f288
Anubhab Guha <anubhabguha1999@gmail.com>
2023-03-08 18:26:01 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h2>This is site is developed by Anubhab Guha</h2>
+            <p><button><a href="https://github.com/anubhabguha1999">Visit gitHub Profile</a></button></p>
+        </div>
+    )
+}
